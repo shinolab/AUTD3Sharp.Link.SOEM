37338246a977af1e56388774c4665116194dd701
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-07-03 08:41:16 +0900

    bump to v35.0.1.1

--- example/SOEM/Program.cs
+++ example/SOEM/Program.cs
@@ -5,7 +5,7 @@ using AUTD3Sharp.Utils;
 using AUTD3Sharp.Link;
 using static AUTD3Sharp.Units;
 
-System.Environment.SetEnvironmentVariable("RUST_LOG", "autd3=INFO");
+Environment.SetEnvironmentVariable("RUST_LOG", "autd3=INFO");
 SOEM.Tracing.Init();
 
 using var autd = Controller.Open([new AUTD3(pos: Point3.Origin, rot: Quaternion.Identity)], new SOEM(
@@ -14,7 +14,7 @@ using var autd = Controller.Open([new AUTD3(pos: Point3.Origin, rot: Quaternion.
                 Console.Error.WriteLine($"slave [{slave}]: {status}");
                 if (status == Status.Lost)
                         // You can also wait for the link to recover, without exiting the process
-                        System.Environment.Exit(-1);
+                        Environment.Exit(-1);
         }, option: new SOEMOption()));
 
 autd.Send((new Sine(freq: 150f * Hz, option: new SineOption()), new Focus(pos: autd.Center() + new Vector3(0f, 0f, 150f), option: new FocusOption())));
